ec55cd17866f65c301d96b3e75e5f175b85669f9
austindriggs <austind26@icloud.com>
2025-10-14 20:04:59 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # install packages
 sudo apt update
-sudo apt install git-lfs hashcat john
+sudo apt install git-lfs hashcat john binwalk aircrack-ng
 
 # install wordlists
 wget -O password_cracking/rockyou/rockyou.txt -c "https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt"
